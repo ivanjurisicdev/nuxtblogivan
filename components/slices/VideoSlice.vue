e0ab90dd7efb66ce00329960d93fcabdeb9502ae
Ivan Jurisic <ivan@iconisagency.com>
2020-10-18 18:00:06 +0200

    Initial commit

---
+++ components/slices/VideoSlice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="post-part single container">
+    <prismic-embed :field="slice.primary.video" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["slice"],
+  name: "text-slice",
+};
+</script>
+
+<style lang="sass">
+.textslice
+  a
+    text-decoration: underline
+</style>
